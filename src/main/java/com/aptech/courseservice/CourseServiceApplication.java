e0eb39050d0b32ef42e18fe309c38546e7b3b8c2
“Godspeare” <godspearejohnson@gmail.com>
2026-06-14 22:56:35 +0100

    fix: ignore all database configurations

--- src/main/java/com/aptech/courseservice/CourseServiceApplication.java
+++ src/main/java/com/aptech/courseservice/CourseServiceApplication.java
@@ -3,11 +3,16 @@ package com.aptech.courseservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        R2dbcAutoConfiguration.class
+})
 public class CourseServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CourseServiceApplication.class, args);
     }
+
 }
